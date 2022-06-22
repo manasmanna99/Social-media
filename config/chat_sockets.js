86f15ee0d73c1d6966245cbf8fcd1e4d6c1eec36
Manas Ranjan Manna <madanmanna123456@gmail.com>
2022-06-23 05:03:12 +0530

    minor

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -2,7 +2,7 @@ module.exports.chatSockets = function (socketServer) {
 
     const io = require('socket.io')(socketServer, {
         cors: {
-            origin: "http://localhost:8000"
+            origin: "http://3.86.149.3:8000"
         }
     });
     io.on('connection', function (socket) {
