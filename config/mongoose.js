44069c5a2d480c78c389f742ebd232c388086366
Manas Ranjan Manna <39239865+manas-manna@users.noreply.github.com>
2023-07-24 07:26:51 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
-mongoose.connect(`mongodb+srv://manas:manas123@cluster0.hha0ca6.mongodb.net/${env.db}`,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
+// Create the connection string.
+const connectionString = `mongodb+srv://manas:manas123@cluster0.hha0ca6.mongodb.net/${env.db}`;
+
+// Connect to the MongoDB Atlas cluster using a promise.
+const promise = mongoose.connect(connectionString, {useNewUrlParser: true});
+
+// Resolve the promise when the connection is established.
+promise.then(() => {
+  console.log("Database Connection established");
 });
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+// Reject the promise if the connection fails.
+promise.catch(err => {
+  console.error(err);
 });
 
-
-module.exports = db;
+module.exports = promise;
