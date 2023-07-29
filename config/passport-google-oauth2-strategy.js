69e1e6ed0a7b5a903c7294801acda9d09a1f0776
Manas Ranjan Manna <39239865+manas-manna@users.noreply.github.com>
2023-07-29 13:40:26 +0530

    passport google bug fix

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -9,29 +9,24 @@ passport.use(new googleStrategy({
     clientSecret: env.google_clientSecret,
     callbackURL: env.google_callbackURL
 },
-function(accessToken, refreshToken, profile, done){
-    User.findOne({email: profile.emails[0].value}).exec(function(err,user){
-        if(err){
-            console.log('error in google strategy-passport',err);
-            return;
-        }
+async function(accessToken, refreshToken, profile, done){
+    try{
+        let user = await User.findOne({email: profile.emails[0].value});
 
         if(user){
             return done(null, user);
         }else{
-            User.create({
+            user = await User.create({
                 name: profile.displayName,
                 email: profile.emails[0].value,
                 password: crypto.randomBytes(20).toString('hex')
-            }, function(err, user){
-                if(err){
-                    console.log('Error', err);
-                    return;
-                }
-                return done(null, user);
             });
+            return done(null, user);
         }
-    });
+    }catch(error){
+        console.log("Error in passport google strategy file",error);
+    }
+    
 }));
 
 module.exports = passport;
