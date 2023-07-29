1d4383db014e3e4305db4de3dfac7b8239dc8dcc
Manas Ranjan Manna <39239865+manas-manna@users.noreply.github.com>
2023-07-29 17:39:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ const customMware = require('./config/middleware');
 //setup the chat server to be used with socket.io
 const chatServer = require('https').Server(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
+chatServer.listen(5000)
 
 // if(env.name == 'development'){
 //     app.use(sassMiddleware({
